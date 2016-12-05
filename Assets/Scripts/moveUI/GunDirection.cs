d204cf80c04f0bc0d7ca54518dbdfc343bad6141
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-12-06 00:50:37 +0900

    照準の色が変わるように変更
    敵に当たると青から赤に変わるように変更

--- Assets/Scripts/moveUI/GunDirection.cs
+++ Assets/Scripts/moveUI/GunDirection.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GunDirection : MonoBehaviour {
 
 	public GameObject aim;
+	public GameObject mazzle;
+	public LayerMask layerMask;
+	public GameObject aimPoint;
+	//public GameObject target;
 
 	void Update () {
 
 		this.transform.LookAt (aim.transform);
 
+		//敵に当たると照準の色が変わる
+		Ray ray = new Ray (mazzle.transform.position, transform.forward);
+		RaycastHit hit;
+		//if (NavMesh.Raycast (mazzle.transform.position, target.transform.position,  out hit, NavMesh.AllAreas)) {
+		if (Physics.SphereCast (ray, 0.8f, out hit, 15.0f, layerMask)) {
+			aimPoint.GetComponent<Image> ().color = new Color (1f, 0.0f, 0.0f, 0.5f);
+		} else {
+			aimPoint.GetComponent<Image> ().color = new Color (0.0f, 1f, 1f, 0.5f);
+		}
 	}
 }
