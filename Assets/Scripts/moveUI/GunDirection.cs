7bcaa15b856c577eec4d8579ec3b4d33271d61bb
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-12-08 01:35:58 +0900

    テストシーン内の敵の位置を移動
    テストのため、敵の位置をNavMeshのエリア内に移動
    不要なオブジェクトをシーン内から削除

--- Assets/Scripts/moveUI/GunDirection.cs
+++ Assets/Scripts/moveUI/GunDirection.cs
@@ -8,23 +8,39 @@ public class GunDirection : MonoBehaviour {
 	public GameObject mazzle;
 	public LayerMask layerMask;
 	public GameObject aimPoint;
-	//public GameObject target;
+	public GameObject target;
 
 	void Update () {
 
 		this.transform.LookAt (aim.transform);
 
+
 		//敵に当たると照準の色が変わる
 		Ray ray = new Ray (mazzle.transform.position, transform.forward);
 		RaycastHit hit;
-
-
-		//if (NavMesh.Raycast (mazzle.transform.position, target.transform.position,  out hit, NavMesh.AllAreas)) {
 
 		if (Physics.Raycast (ray, out hit, 15.0f, layerMask)) {
 			aimPoint.GetComponent<Image> ().color = new Color (1f, 0.0f, 0.0f, 0.5f);
 		} else {
 			aimPoint.GetComponent<Image> ().color = new Color (0.0f, 1f, 1f, 0.5f);
 		}
+
+
+	/*
+		//とりあえずtargetは手動で設定してテスト　(取得方法は後で)
+		NavMeshHit Hit;
+		if (NavMesh.Raycast (mazzle.transform.position, target.transform.position, out Hit, NavMesh.AllAreas)) {
+			Debug.Log ("障害物あり");
+			//targetまでたどり着かなければ青
+			aimPoint.GetComponent<Image> ().color = new Color (0f, 1f, 1f, 0.5f);
+
+		} else {
+			Debug.Log ("障害物なし");
+			//targetまでたどり着けば赤
+			aimPoint.GetComponent<Image> ().color = new Color (1f, 0f, 0f, 0.5f);
+		}
+
+		*/
+	
 	}
 }
