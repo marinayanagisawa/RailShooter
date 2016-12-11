872ffe2e9d7866b5d0fff5b251d4e3d2df1a7c59
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-12-11 14:14:06 +0900

    HPゲージを追加
    プレイヤーのHPゲージを追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-	public int hp = 3;
-	GameObject enemyShot;
-
+	public int hp;
+	public Slider slider;
 
 	void Start () {
-		//ゲームフラグを確認してから移動スタート
+		//--------------ゲームフラグを確認してから移動スタートさせる
+
+		slider.GetComponent<Slider>();
+		hp = 3;
 	
+
+
 	}
 	
 	void Update () {
-	
+		//HPを表示
+		slider.value = hp;
 	}
 
 
 	void OnTriggerEnter(Collider col) {
 
 		//enemyShotとヒットしたときの処理（hp--,hp残り判定,音、エフェクト）
-		//ただし、弾が飛ぶ、画面揺れなどは単に演出にしてHPを引く場合はこのメソッド自体いらない
+
+		if (hp > 0) {
+			hp--;
+		}
+
+
+		
 	}
 
 
