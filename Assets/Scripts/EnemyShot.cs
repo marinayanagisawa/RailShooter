dadfd2428af52c24f8c26a788891ab7df2343408
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-12-11 14:10:15 +0900

    ショットがプレイヤーを追尾するように変更

--- Assets/Scripts/EnemyShot.cs
+++ Assets/Scripts/EnemyShot.cs
@@ -3,25 +3,29 @@ using System.Collections;
 
 public class EnemyShot : MonoBehaviour {
 
-	private float shotSpeed = 0.1f;
-	private float lifeTime = 9.0f;
+	private float shotSpeed = 3.5f;
+	private float lifeTime = 2.0f;
+	private GameController gc;
+	private int score = 100;
 	//private GameObject player;
 
 	void Start(){
 		//player = GameObject.Find ("Player");
-		//this.transform.LookAt (player.transform);
-		this.transform.LookAt (Camera.main.transform);
+		gc = GameObject.Find("GameController").GetComponent<GameController>();
+		
 	}
 
 	void Update () {
 
-		this.transform.Translate (0, 0, shotSpeed);
+		this.transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, shotSpeed * Time.deltaTime);
 		Destroy (this.gameObject, lifeTime);
 	}
 
+	
 	void OnTriggerEnter(Collider col){
 
 		Destroy (this.gameObject);
 
 	}
+	
 }
