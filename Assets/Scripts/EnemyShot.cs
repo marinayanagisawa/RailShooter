a718888078d593018c1b6cbdb3b81c9f6f6b6bc2
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-12-13 01:15:20 +0900

    ショットの相殺とスコア加算を追加

--- Assets/Scripts/EnemyShot.cs
+++ Assets/Scripts/EnemyShot.cs
@@ -5,13 +5,14 @@ public class EnemyShot : MonoBehaviour {
 
 	private float shotSpeed = 1.5f;
 	private float lifeTime = 8.0f;
-	//private GameController gc;
-	private int score = 100;
+	private GameController gc;
+	private int score = 1000;
 	//private GameObject player;
+	public GameObject par;
 
 	void Start(){
 		//player = GameObject.Find ("Player");
-		//gc = GameObject.Find("GameController").GetComponent<GameController>();
+		gc = GameObject.Find("GameController").GetComponent<GameController>();
 		Destroy(this.gameObject, lifeTime);
 
 	}
@@ -24,9 +25,19 @@ public class EnemyShot : MonoBehaviour {
 
 	
 	void OnTriggerEnter(Collider col){
+		string layerName = LayerMask.LayerToName(col.gameObject.layer);
 
-		Destroy (this.gameObject);
-
+		if (layerName == "PlayerShot") {
+			PlayParticle (col.gameObject);
+			Destroy (this.gameObject);
+			gc.addScore (score);
+		}
 	}
-	
+
+
+	void PlayParticle(GameObject shot){
+		Object parObj = Instantiate (par, shot.transform.position, shot.transform.rotation);
+		Destroy (parObj, 1.5f);
+	}
+
 }
