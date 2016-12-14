7a2a6a71c0066698a405d541c9c8d1cf59bee04c
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-12-14 18:51:58 +0900

    カメラの自転を追加
    敵の方向を向かせるための準備
    キーパッドで方向を変えながらテストプレイできるように変更

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,6 +22,10 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		//HPを表示
 		slider.value = hp;
+
+		//ステージ作成時のテスト用
+		RotateTest ();
+
 	}
 
 
@@ -34,12 +38,42 @@ public class PlayerController : MonoBehaviour {
 		if (hp > 0) {
 			hp--;
 		}
-
-
 		
 	}
+		
 
+	public void CameraTurn(int euler){
+		//Y軸のプラス（マイナス）に進む時は,0（180）で正面,90（270）で右向き,270（90）で左向き
+		//X軸のプラス（マイナス）に進む時は,90（270）で正面,180（0）で右向き,0（180）で左向き
+		iTween.RotateTo (cam, iTween.Hash ("y", euler, "time", 3.0f));
+	}
 
-	//プレイヤーのカメラ回転メソッド（必要に応じて）
+	//検証用の方向指示（テスト専用）
+	void RotateTest(){
+		if (Input.GetKeyDown (KeyCode.Keypad8)) {
+			CameraTurn (0);
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad2)) {
+			CameraTurn (180);
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad6)) {
+			CameraTurn (90);
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad4)) {
+			CameraTurn (270);
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad9)) {
+			CameraTurn (45);
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad3)) {
+			CameraTurn (135);
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad1)) {
+			CameraTurn (225);
+		}
+		if (Input.GetKeyDown (KeyCode.Keypad7)) {
+			CameraTurn (315);
+		}
+	}
 
 }
