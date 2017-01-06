677edaea9d4293a57a69d76aa3c759f31454a6ee
marinayanagisawa <marinayanagisawacb@gmail.com>
2017-01-06 17:51:11 +0900

    ドアに音を追加

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 	public Animator anim;
+	public AudioSource sound;
 
 	void OnTriggerEnter(Collider col){
 		string layerMask = LayerMask.LayerToName (col.gameObject.layer);
@@ -10,6 +11,14 @@ public class Door : MonoBehaviour {
 		if (layerMask == "Player") {
 			Debug.Log ("DoorOpen!");
 			anim.SetTrigger ("DoorOpen");
+
+			Invoke("OpenSound", 0.1f);
 		}
 	}
+
+
+	public void OpenSound(){
+		sound.PlayOneShot (sound.clip);
+	}
+
 }
