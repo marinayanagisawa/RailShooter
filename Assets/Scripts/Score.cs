19ec86ad6f0681e499cc4ccad00984ed0add8d5f
marinayanagisawa <marinayanagisawacb@gmail.com>
2017-01-10 15:45:20 +0900

    新しいランキングパネル用にスクリプトを修正
    Score.csを修正、整理
    ※ハイスコアメッセージがハイスコア時に赤くならないので確認

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// スコアランキングのスクリプト
+/// ranking配列には,　スコア,　評価（int）,　命中率 の順で結合したstringを格納
+/// </summary>
+
 public class Score : MonoBehaviour {
-
-	//ここに取り出したスコアを入れる(最後の１つは今回のスコア用に確保)
-	//private int[] rankingScore = new int[6];
-
 	
 	//文字列結合してランキングデータを入れる(最後の１つは今回のスコア用に確保)
 	private string[] ranking = new string[6];
@@ -17,6 +18,8 @@ public class Score : MonoBehaviour {
 	private string[] scoreKey = new string[5];
 
 	//Canvasから紐づけ
+	public Text[] score = new Text[5];
+	public Text[] hitRate = new Text[5];
 	public Text[] rank = new Text[5];
 	public Text currentScore;
 
@@ -26,35 +29,21 @@ public class Score : MonoBehaviour {
 		for (int i = 0; i < 5; i++) {
 			scoreKey [i] = "score" + i;
 		}
-		/*
-		//PlayerPrefsの中身を配列に取り出す. 入っていなければ０が返る
-		for (int i = 0; i < 5; i++) {
-			rankingScore [i] = PlayerPrefs.GetInt (scoreKey[i],0);
-			Debug.Log (rankingScore[i]);
-		}
-*/
 
         //rankingに保存データを取り出し（String）
 		for (int i = 0; i < 5; i++) {
-			ranking[i] = PlayerPrefs.GetString(scoreKey[i],"0,0");
+			ranking[i] = PlayerPrefs.GetString(scoreKey[i],"0,E,0");
 			Debug.Log(ranking[i]);
 		}
-
-
-
+			
 		//前回保存されたスコアを,文字列からintに直して取っておく
 		for (int i = 0; i < 5; i++) {
 			//lastRankingScore[i] = rankingScore[i];
 			lastRankingScore[i] = int.Parse(ranking[i].Split(',')[0]);
 		}
 
-		//今回のスコアを配列の最後に追加
-		//rankingScore [5] = LocalValues.totalScore;
-
 		//カンマ区切りで今回のスコアと評価を配列の最後に追加
-		ranking[5] = LocalValues.totalScore + "," + LocalValues.rankNum;
-
-
+		ranking[5] = LocalValues.totalScore + "," + LocalValues.rank + "," +LocalValues.hitRate;
 
 		//ranking内を降順でソート（文字列からintに直しつつ比較,文字列のままソート）
 		for (int i = 0; i < ranking.Length - 1; i++) {
@@ -74,9 +63,10 @@ public class Score : MonoBehaviour {
 		int rankingNum = 0;
 		bool changeRank = false;
 		for (int i = 0; i < 5; i++) {
+			
 			//順位の変更を見つけたらrankingNumに順位を入れる
 			if (!changeRank) {
-				//if (rankingScore [i] != lastRankingScore [i]) {
+				
 				if (int.Parse(ranking[i].Split(',')[0]) != lastRankingScore[i]) {
 					rankingNum = i;
 					Debug.Log ((rankingNum+1) + "位にランクイン！");
@@ -84,32 +74,32 @@ public class Score : MonoBehaviour {
 				}
 			}
 		}
-
-		/*
-		//確認用
-		for (int i = 0; i < 6; i++) {
-			Debug.Log ("key番号" + i +";" +rankingScore [i]);
-		}
-		*/
+			
 
 		//ランキング順位に変更があったら文字色を変える
 		if (changeRank) {
+			score [rankingNum].GetComponent<Text> ().color = Color.red;
 			rank [rankingNum].GetComponent<Text> ().color = Color.red;
+			hitRate [rankingNum].GetComponent<Text> ().color = Color.red;
+			score [rankingNum].transform.parent.GetComponent<Image> ().color = Color.red;  //枠
+		}
+			
+		//スコアを表示
+		score [0].text = "1st   " + ranking [0].Split (',') [0];
+		score [1].text = "2nd   " + ranking [1].Split (',') [0];
+		score [2].text = "3rd   " + ranking [2].Split (',') [0];
+		score [3].text = "4th   " + ranking [3].Split (',') [0];
+		score [4].text = "5th   " + ranking [4].Split (',') [0];
+
+		//命中率を表示
+		for (int i = 0; i < 5; i++) {
+			hitRate [i].text = ranking [i].Split (',') [2] + "%";
 		}
 
-		//rankingScoreの上位5件までと,今回のスコアをUI表示する
-	/*	rank[0].text = "1st   " + rankingScore[0];
-		rank[1].text = "2nd   " + rankingScore[1];
-		rank[2].text = "3rd   " + rankingScore[2];
-		rank[3].text = "4th   " + rankingScore[3];
-		rank[4].text = "5th   " + rankingScore[4];
-*/
-		rank[0].text = "1st   " + ranking[0].Split(',')[0] + "  " + LocalValues.rank;
-		rank[1].text = "2nd   " + ranking[1].Split(',')[0] + "  " + LocalValues.rank;
-		rank[2].text = "3rd   " + ranking[2].Split(',')[0] + "  " + LocalValues.rank;
-		rank[3].text = "4th   " + ranking[3].Split(',')[0] + "  " + LocalValues.rank;
-		rank[4].text = "5th   " + ranking[4].Split(',')[0] + "  " + LocalValues.rank;
-
+		//評価を表示
+		for (int i = 0; i < 5; i++) {
+			rank [i].text = ranking [i].Split (',') [1];
+		}
 
 
 		//1位だったら赤字で表示
@@ -122,7 +112,7 @@ public class Score : MonoBehaviour {
 
 		//配列の中身をplayerPrefsに保存する
 		for (int i = 0; i < 5; i++) {
-			//PlayerPrefs.SetInt(scoreKey[i], rankingScore[i] );
+			
 			PlayerPrefs.SetString(scoreKey[i], ranking[i]);
 			PlayerPrefs.Save ();
 		}
